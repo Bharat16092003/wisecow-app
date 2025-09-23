3d82a4b342f21a78c9063ac2a38a51ea02a8e2f4
Bharat16092003 <singh0077deepu@gmail.com>
2025-09-23 21:15:33 +0530

    Create logfile_analyzer.sh

---
+++ logfile_analyzer.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+
+LOG_FILE="/var/log/apache2/access.log"
+
+
+echo " ------------------------------------------ "
+echo " Web Server Log Analysis Report "
+echo " Generated on $(date) "
+echo " Log File: $LOG_FILE "
+echo " ------------------------------------------ "
+echo
+
+echo " 1. Total 404 Errors: "
+grep "404" "$LOG_FILE" | wc -l
+echo 
+
+echo " 2. Top 5 Requested Pages: "
+awk '{print $7}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5
+echo 
+
+echo " 3. Top 5 IP Addresses "
+awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5
+echo 
+
+
+echo "---------  End of Analysis Report  ---------"
+
